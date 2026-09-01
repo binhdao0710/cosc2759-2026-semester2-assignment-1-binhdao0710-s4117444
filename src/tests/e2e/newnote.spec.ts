e7c46d89c36de8eedb7c15f1b0aa9daa07fa1a10
Binh Dao <s4117444@rmit.edu.vn>
2026-09-01 16:35:21 +1000

    Implement end-to-end testing
    * add end-to-end testing job with artefact generation on error
    
    * set action timeout, base URL, and trace
    
    * Fix syntax issue
    
    * fix playwright compatibility issues
    
    * add a test to check for logging capabilities when a test fail
    
    * comment out failure test

--- src/tests/e2e/newnote.spec.ts
+++ src/tests/e2e/newnote.spec.ts
@@ -1,32 +1,32 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('test', async ({ page }) => {
-
+test("test", async ({ page }) => {
   // Go to http://localhost:3000/
-  await page.goto('http://localhost:3000/');
+  await page.goto("http://localhost:3000/");
+
+  //await expect(page).toHaveURL("http://localhost:3000/this-should-fail");
 
   // Click text=New Note
-  await page.locator('text=New Note').click();
-  await expect(page).toHaveURL('http://localhost:3000/new');
+  await page.locator("text=New Note").click();
+  await expect(page).toHaveURL("http://localhost:3000/new");
 
   // Click [placeholder="Enter Note Title"]
   await page.locator('[placeholder="Enter Note Title"]').click();
 
   // Fill [placeholder="Enter Note Title"]
-  await page.locator('[placeholder="Enter Note Title"]').fill('E2E Test');
+  await page.locator('[placeholder="Enter Note Title"]').fill("E2E Test");
 
   // Press Tab
-  await page.locator('[placeholder="Enter Note Title"]').press('Tab');
+  await page.locator('[placeholder="Enter Note Title"]').press("Tab");
 
   // Fill textarea[name="description"]
-  await page.locator('textarea[name="description"]').fill('E2E Description');
+  await page.locator('textarea[name="description"]').fill("E2E Description");
 
   // Click text=Save
-  await page.locator('text=Save').click();
-  await expect(page).toHaveURL('http://localhost:3000/');
+  await page.locator("text=Save").click();
+  await expect(page).toHaveURL("http://localhost:3000/");
 
   // Click text=Delete
-  await page.locator('text=Delete').click();
-  await expect(page).toHaveURL('http://localhost:3000/');
-
+  await page.locator("text=Delete").click();
+  await expect(page).toHaveURL("http://localhost:3000/");
 });
